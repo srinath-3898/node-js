461a472b24d165c67be3d5569650bbe5b6f1ad98
srinath-3898 <srinath3898@gmail.com>
2023-08-19 21:54:28 +0530

    Task 8 completed

--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 const http = require("http");
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === "/") {
     res.setHeader("Content-type", "text/html");
     res.write("<html>");
     res.write("<head><title>Enter message</title></head>");
+
+    try {
+      const content = await fs.promises.readFile("message.txt", "utf8");
+      res.write(`<body><p>${content}</p>`);
+    } catch (err) {
+      res.write(`<body><p>Error reading file</p>`);
+    }
+
     res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>"
+      `<form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>`
     );
     res.write("</html>");
     return res.end();
   }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
   if (url === "/home") {
     res.setHeader("Content-type", "text/html");
     res.write("<html>");
@@ -22,6 +47,7 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
+
   if (url === "/about") {
     res.setHeader("Content-type", "text/html");
     res.write("<html>");
@@ -30,6 +56,7 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
+
   if (url === "/node") {
     res.setHeader("Content-type", "text/html");
     res.write("<html>");
@@ -38,12 +65,7 @@ const server = http.createServer((req, res) => {
     res.write("</html>");
     return res.end();
   }
-  if (url === "/message" && method === "POST") {
-    fs.writeFileSync("messgae.txt", "Dummy text");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
+
   res.setHeader("Content-type", "text/html");
   res.write("<html>");
   res.write("<head><title>My first page</title></head>");
