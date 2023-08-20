ce2d3b79316044d9e282ca6df1b99d905bba7a71
srinath-3898 <srinath3898@gmail.com>
2023-08-20 11:56:28 +0530

    Added add-product route and product route

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("hello");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res) => {
+  return res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'/><input type='number' name='size'/><button ty'>Add product</button></form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("hii");
-  res.send("<h1>Hello from express</h1>");
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res) => {
+  return res.send("<h1>Hello from express</h1>");
 });
 
 app.listen(4000);
